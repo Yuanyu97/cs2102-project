363ad0d823984b7a144144f1350fdd5d6b91538f
Yuanyu97 <e0322803@u.nus.edu>
2021-04-03 21:00:40 +0800

    edit schema and function

--- server/init.sql
+++ server/init.sql
@@ -182,11 +182,10 @@ CREATE TABLE Conducts (
   rid INTEGER NOT NULL,
   sid INTEGER,
   course_id INTEGER,
-  launch_date DATE,
   FOREIGN KEY (iid, area_name) REFERENCES Instructors,
   FOREIGN KEY (rid) REFERENCES Rooms,
-  FOREIGN KEY (sid, course_id) REFERENCES Sessions,
-  FOREIGN KEY (course_id, launch_date) REFERENCES Offerings,
+  FOREIGN KEY (sid, course_id) REFERENCES Sessions
+    ON DELETE CASCADE,
   PRIMARY KEY (iid, area_name, rid, sid, course_id)
 );
 
